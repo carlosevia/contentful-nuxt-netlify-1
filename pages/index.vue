4432748541edb4e2b7cce9a290deceeb67b606c4
Carlos Evia <cevia@vt.edu>
2020-07-14 09:43:21 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <div class="container">
-      <h1 class="title">Timothy is a n00b</h1>
+      <h1 class="title">Life is good... mostly</h1>
       <section v-for="post in posts" :key="post.fields.slug">
         <div
           class="image"
